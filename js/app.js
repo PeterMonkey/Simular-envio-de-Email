d4694351a757bbbe18aebdd7b80c8b104d063687
unknown <pefernandez1408@gmail.com>
2020-04-15 22:02:59 -0430

    Validando que los campos no esten vacios

--- js/app.js
+++ js/app.js
@@ -12,12 +12,61 @@ eventListener();
 function eventListener(){
     //Inicio de la aplicacion y desabilita submit
     document.addEventListener('DOMContentLoaded', inicioApp);
+
+    //Campos del formulario
+    email.addEventListener('blur', validarCampo);
+    asunto.addEventListener('blur', validarCampo);
+    mensaje.addEventListener('blur', validarCampo);
 }
 
 
 
 //Funciones
+
 function inicioApp(){
     //desabilitar el envio
     btnEnviar.disabled = true;
+}
+
+//Validar que el campo tenga algo escrito
+
+function validarCampo(){
+   // Se valida la longitud del texto y que no este vacio
+   validarLongitud(this);
+
+   //Valiadar unicamente el email
+   //console.log(this.type);
+   if(this.type === 'email'){
+       validarEmail(this);
+   }
+
+   let errores = document.querySelectorAll('.error');
+   if(email.value !== '' && asunto.value !== '' && mensaje.value !== ''){
+       
+        if(errores.length === 0){
+            btnEnviar.disabled = false;
+        }
+   }
+}
+
+function validarLongitud(campo){
+       if(campo.value.length > 0){
+           campo.style.borderBottomColor = 'green';
+           campo.classList.remove('error');
+       }else{
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+       }
+       
+}
+
+function validarEmail(campo){
+   const mensaje = campo.value;
+   if(mensaje.indexOf('@') !== -1){
+    campo.style.borderBottomColor = 'green';
+    campo.classList.remove('error');
+   }else{
+    campo.style.borderBottomColor = 'red';
+    campo.classList.add('error');
+   }
 }
